f8dc3a7139eaa3c56ac5c94d1a626d19d60c6cac
David Charte <fdavidcl@outlook.com>
2014-03-08 00:02:44 +0100

    Macros guapas

---
+++ macro.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+i=10
+[[ $# -lt 2 ]] && echo "Sintaxis: $0 <nombre_programa>" && exit -1
+touch $1.dat
+while [[ $i -lt 10000 ]]; do
+	echo -n "$i | "
+	echo -n "$i " >> $1.dat
+	./$1 $i >> $1.dat
+	let i=$i+10
+done
