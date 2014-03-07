56930c7095141211a82eb14f7cb0ff40b151e084
NCordon <nacho.cordon.castillo@gmail.com>
2014-03-07 23:52:54 +0100

    Burbuja y fibonacci operativos

--- burbuja.cpp
+++ burbuja.cpp
@@ -1,61 +1,50 @@
 /**
-@file Ordenación por burbuja
-*/
+ * @file Ordenación por burbuja
+ */
 
 #include <iostream>
-using namespace std;
 #include <ctime>
 #include <cstdlib>
 #include <climits>
 #include <cassert>
 #include <ctime>
 #include <unistd.h>
+using namespace std;
 
 #define NUM_VECES 10000
 
-
-
-/* ************************************************************ */
-/* Método de ordenación por burbuja */
-
 /**
-@brief Ordena un vector por el método de la burbuja.
-
-@param T: vector de elementos. Debe tener num_elem elementos.
-Es MODIFICADO.
-@param num_elem: número de elementos. num_elem > 0.
-
-Cambia el orden de los elementos de T de forma que los dispone
-en sentido creciente de menor a mayor.
-Aplica el algoritmo de la burbuja.
-*/
+ * @brief Ordena un vector por el método de la burbuja.
+ * @param T: vector de elementos. Debe tener num_elem elementos.
+ * Es modificado.
+ * @param num_elem: número de elementos. num_elem > 0.
+ * 
+ * Cambia el orden de los elementos de T de forma que los dispone
+ * en sentido creciente de menor a mayor.
+ * Aplica el algoritmo de la burbuja.
+ */
 
 inline static void burbuja(int T[], int num_elem);
 
 /**
-@brief Ordena parte de un vector por el método de la burbuja.
-
-@param T: vector de elementos. Tiene un número de elementos
-mayor o igual a final.Es MODIFICADO.
-
-@param inicial: Posición que marca el incio de la parte del
-vector a ordenar.
-@param final: Posición detrás de la última de la parte del
-vector a ordenar.
-inicial < final.
-
-Cambia el orden de los elementos de T entre las posiciones
-inicial y final - 1de forma que los dispone en sentido creciente
-de menor a mayor.
-Aplica el algoritmo de la burbuja.
-*/
+ * @brief Ordena parte de un vector por el método de la burbuja.
+ * @param T: vector de elementos. Tiene un número de elementos
+ * mayor o igual a final.Es MODIFICADO.
+ * @param inicial: Posición que marca el incio de la parte del
+ * vector a ordenar.
+ * @param final: Posición detrás de la última de la parte del
+ * vector a ordenar.
+ * inicial < final.
+ * 
+ * Cambia el orden de los elementos de T entre las posiciones
+ * inicial y final - 1de forma que los dispone en sentido creciente
+ * de menor a mayor.
+ * Aplica el algoritmo de la burbuja.
+ */
 
 static void burbuja_lims(int T[], int inicial, int final);
 
-/**
-Implementación de las funciones
-**/
-
+// Implementación
 
 inline void burbuja(int T[], int num_elem){
     burbuja_lims(T, 0, num_elem);
@@ -74,6 +63,14 @@ static void burbuja_lims(int T[], int inicial, int final){
             }
 }
 
+/**
+ * @brief Permite duplicar un vector de enteros
+ * @param T puntero a un vector de enteros
+ * @param U puntero a otro vector de enteros
+ * @param n tamanio de ambos vectores
+ * @pre Han de tener el mismo tamanio
+ */
+
 void duplicaVector(int* T,int* U,int tam){
     for (int i=0; i<tam; ++i){
         U[i]=T[i];
@@ -87,7 +84,6 @@ int main(int argc, char* argv[]){
     }
     int n = atoi(argv[1]);
     int * T = new int[n], *U=new int[n];
-    double t_transcurrido;
     clock_t t_antes, t_despues, t_a, t_b(0);
     
     srandom(time(0));
@@ -96,7 +92,7 @@ int main(int argc, char* argv[]){
         T[i] = random();
     }
     
-    t_antes = clock();
+    t_antes = clock();    
     for (int i=0; i<NUM_VECES; ++i){
         t_a=clock();
         duplicaVector(T,U,n);
@@ -104,6 +100,7 @@ int main(int argc, char* argv[]){
         burbuja(U, n);
     }
     t_despues = clock();
+    
     delete [] T;
     delete [] U;
     
--- fibonacci.cpp
+++ fibonacci.cpp
@@ -1,19 +1,19 @@
 /**
- *   @file Cálculo de la sucesión de Fibonacci
+ * @file Cálculo de la sucesión de Fibonacci
  */
 
 #include <iostream>
-using namespace std;
 #include <ctime>
+using namespace std;
 
+#define NUM_VECES 10000
 
 /**
- *   @brief Calcula el término n-ésimo de la sucesión de Fibonacci.
- * 
- *   @param n: número de orden del término buscado. n >= 1.
- * 
- *   @return: término n-ésimo de la sucesión de Fibonacci.
+ * @brief Calcula el término n-ésimo de la sucesión de Fibonacci.
+ * @param n: número de orden del término buscado. n >= 1.
+ * @return: término n-ésimo de la sucesión de Fibonacci.
  */
+
 int fibo(int n){
     if (n < 2)
         return 1;
@@ -21,18 +21,19 @@ int fibo(int n){
         return fibo(n-1) + fibo(n-2);
 }
 
+int main(int argc, char* argv[]){
+    if (argc !=2){
+        cerr << "Uso del programa: " + (string)(argv[0]) + " <número entero>" << endl;  
+        return -1;
+    }
+    int n = atoi(argv[1]);
+    clock_t t_antes, t_despues
+    
+    t_antes = clock();
+    for (int i=0; i<NUM_VECES; ++i){
+        fib(n);
+    t_despues = clock();
 
-int main(){
-    int n;
-    int f;
-    
-    cout << "¿Número del término: ";
-    cin >> n;
-    
-    f = fibo(n);
-    
-    cout << "El término " << n << "-ésimo es: " << f << endl;
-    
+    cout << (double)(t_despues-t_antes)/(CLOCKS_PER_SEC);
     return 0;
 }
-
