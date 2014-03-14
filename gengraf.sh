c3c43a8633f1a8631e846852ec0619ed6cd06202
M42 <mromang08@gmail.com>
2014-03-14 08:55:23 +0100

    Comentarios a gengraf.sh.

--- gengraf.sh
+++ gengraf.sh
@@ -3,12 +3,17 @@
 # Variables de ejecución
 SCRIPT=plot
 SOURCES=(burbuja fibonacci floyd hanoi heapsort insercion mergesort quicksort seleccion)
+# Límite e incremento de tamaño.
 LIMITS=(10000 50 1000 35 10000 10000 10000 10000 10000)
 INC=(100 1 5 1 100 100 100 100 100)
 N_ITER=5
 
 
-        
+# Genera todos los ejecutables.
+make
+
+# Para cada uno de los programas, ejecuta hasta el límite con el incremento dado.
+# Vuelca los datos obtenidos en un .dat y genera el gráfico con gnuplot. 
 for i in `seq 0 $((${#SOURCES[*]}-1))`
 do
     src=${SOURCES[$i]}
@@ -16,10 +21,8 @@ do
  
     echo -n "" > $src.dat
     [[ $inc -eq 1 ]] && j=1 || j=10
-    
-    g++ ./$src.cpp -o $src
 
-    while [[ $j -lt ${LIMITS[$i]} ]]; 
+    while [[ $j -lt ${LIMITS[$i]} ]];
     do
         echo -n "$j " >> $src.dat
         sum=0
